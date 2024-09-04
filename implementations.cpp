ba89f24b8ef341729bdb72b15242b6bb85a62fbf
suny-am <visualarea.1@gmail.com>
2024-09-04 19:59:27 +0200

    update implementation definitions

--- implementations.cpp
+++ implementations.cpp
@@ -5,6 +5,15 @@
 // TinyOBJLoader
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "resource-loaders/tiny_obj_loader.h"
+// TinyGLTF
+#define TINYGLTF_IMPLEMENTATION
+#include "resource-loaders/tiny_gltf.h"
 // STB Image 
 #define STB_IMAGE_IMPLEMENTATION
 #include "resource-loaders/stb_image.h"
+// STB Image Write
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "resource-loaders/stb_image_write.h"
+// WebGPU Standard Utils
+#define WEBGPU_STD_UTILS_IMPLEMENTATION
+#include "webgpu-std-utils.hpp"
