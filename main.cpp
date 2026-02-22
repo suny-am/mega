b111a8829928fae5ed4a117f42f87810fd4c5976
suny-am <visualarea.1@gmail.com>
2026-02-22 13:02:41 +0100

    step010: complete

--- main.cpp
+++ main.cpp
@@ -7,6 +7,40 @@
 #endif // __EMSCRIPTEN__
 #include <iostream>
 
+WGPUDevice requestDeviceSync(WGPUAdapter adapter,
+                             WGPUDeviceDescriptor const *descriptor) {
+  struct UserData {
+    WGPUDevice device = nullptr;
+    bool requestEnded = false;
+  };
+  UserData userData;
+
+  auto onDeviceRequestEnded = [](WGPURequestDeviceStatus status,
+                                 WGPUDevice device, char const *message,
+                                 void *pUserData) {
+    UserData &userData = *reinterpret_cast<UserData *>(pUserData);
+    if (status == WGPURequestDeviceStatus_Success) {
+      userData.device = device;
+    } else {
+      std::cout << "Could not get WebGPU device: " << message << std::endl;
+    }
+    userData.requestEnded = true;
+  };
+
+  wgpuAdapterRequestDevice(adapter, descriptor, onDeviceRequestEnded,
+                           (void *)&userData);
+
+#ifdef __EMSCRIPTEN__
+  while (!userData.requestEnded) {
+    emscripten_sleep(100);
+  }
+#endif
+
+  assert(userData.requestEnded);
+
+  return userData.device;
+}
+
 WGPUAdapter requestAdapterSync(WGPUInstance instance,
                                WGPURequestAdapterOptions const *options) {
   struct UserData {
@@ -40,6 +74,42 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance,
   assert(userData.requestEnded);
 
   return userData.adapter;
+}
+
+void inspectDevice(WGPUDevice device) {
+  std::vector<WGPUFeatureName> features;
+  size_t featureCount = wgpuDeviceEnumerateFeatures(device, nullptr);
+  features.resize(featureCount);
+  wgpuDeviceEnumerateFeatures(device, features.data());
+
+  std::cout << "Device features:" << std::endl;
+  std::cout << std::hex;
+  for (auto f : features) {
+    std::cout << " - 0x" << f << std::endl;
+  }
+  std::cout << std::dec;
+
+  WGPUSupportedLimits limits = {};
+  limits.nextInChain = nullptr;
+
+#ifdef WEBGPU_BACKEND_DAWN
+  bool success = wgpuDeviceGetLimits(device, &limits) == WGPUStatus_Success;
+#else
+  bool success = wgpuDeviceGetLimits(device, &limits);
+#endif
+
+  if (success) {
+    std::cout << "Device limits:" << std::endl;
+    std::cout << " - maxTextureDimension1D: "
+              << limits.limits.maxTextureDimension1D << std::endl;
+    std::cout << " - maxTextureDimension2D: "
+              << limits.limits.maxTextureDimension2D << std::endl;
+    std::cout << " - maxTextureDimension3D: "
+              << limits.limits.maxTextureDimension3D << std::endl;
+    std::cout << " - maxTextureArrayLayers: "
+              << limits.limits.maxTextureArrayLayers << std::endl;
+    // [...] Extra device limits
+  }
 }
 
 void inspectAdapter(WGPUAdapter adapter) {
@@ -113,24 +183,37 @@ int main(int, char **) {
   WGPUInstanceDescriptor desc = {};
   desc.nextInChain = nullptr;
 
-  // We create the instance using this descriptor
+#ifdef WEBGPU_BACKEND_DAWN
+  // Make sure the uncaptured error callback is called as soon as an error
+  // occurs rather than at the next call to "wgpuDeviceTick".
+  WGPUDawnTogglesDescriptor toggles;
+  toggles.chain.next = nullptr;
+  toggles.chain.sType = WGPUSType_DawnTogglesDescriptor;
+  toggles.disabledToggleCount = 0;
+  toggles.enabledToggleCount = 1;
+  const char *toggleName = "enable_immediate_error_handling";
+  toggles.enabledToggles = &toggleName;
+
+  desc.nextInChain = &toggles.chain;
+#endif // WEBGPU_BACKEND_DAWN
+
+  // Create instance
 #ifdef WEBGPU_BACKEND_EMSCRIPTEN
   WGPUInstance instance = wgpuCreateInstance(nullptr);
 #else  //  WEBGPU_BACKEND_EMSCRIPTEN
   WGPUInstance instance = wgpuCreateInstance(&desc);
 #endif //  WEBGPU_BACKEND_EMSCRIPTEN
 
-  // We can check whether there is actually an instance created
+  // Validation
   if (!instance) {
     std::cerr << "Could not initialize WebGPU!" << std::endl;
     return 1;
   }
 
-  // Display the object (WGPUInstance is a simple pointer, it may be
-  // copied around without worrying about its size).
+  // Inspect instance
   std::cout << "WGPU instance: " << instance << std::endl;
 
-  std::cout << "Request adaptAer..." << std::endl;
+  std::cout << "Requesting adapter..." << std::endl;
 
   WGPURequestAdapterOptions adapterOpts = {};
   adapterOpts.nextInChain = nullptr;
@@ -140,10 +223,47 @@ int main(int, char **) {
 
   inspectAdapter(adapter);
 
-  // We clean up the WebGPU instance
-  wgpuInstanceRelease(instance);
+  std::cout << "Requesting device..." << std::endl;
 
+  WGPUDeviceDescriptor deviceDesc = {};
+  deviceDesc.nextInChain = nullptr;
+  deviceDesc.label = "My Device";      // TODO: set to adapter deviceID
+  deviceDesc.requiredFeatureCount = 0; // we do not require any specific feature
+  deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+  deviceDesc.defaultQueue.nextInChain = nullptr;
+  deviceDesc.defaultQueue.label = "The default queue";
+  // Good for debugging
+  deviceDesc.deviceLostCallback = [](WGPUDeviceLostReason reason,
+                                     char const *message,
+                                     void * /* pUserData */) {
+    std::cout << "Device lost: reason " << reason;
+    if (message)
+      std::cout << " (" << message << ")";
+    std::cout << std::endl;
+  };
+  WGPUDevice device = requestDeviceSync(adapter, &deviceDesc);
+
+  std::cout << "Got device: " << device << std::endl;
+
+  // Once we have the device, we no longer need the adapter
   wgpuAdapterRelease(adapter);
+
+  inspectDevice(device);
+
+  // Good for debugging
+  auto onDeviceError = [](WGPUErrorType type, char const *message,
+                          void * /* pUserData */) {
+    std::cout << "Uncaptured device error: type " << type;
+    if (message)
+      std::cout << " (" << message << ")";
+    std::cout << std::endl;
+  };
+  wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError,
+                                       nullptr /* pUserData */);
+
+  // Clean up
+  wgpuInstanceRelease(instance);
+  wgpuDeviceRelease(device);
 
   return 0;
 }
